a80616414a3ce1a5fa3cc0c452a33327396bdf07
losandluca <lewisalcazar88@gmail.com>
2019-02-06 21:42:24 -0700

    added function to display data to terminal in a table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer") ;
 const mysql = require("mysql");
 const  table = require("cli-table");
 
+//connections to mysql database and
 const connection = mysql.createConnection( {
     host: "localhost",
     port: 3500,
@@ -13,4 +14,16 @@ const connection = mysql.createConnection( {
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-})
+
+    readProducts();
+});
+
+//function to display data into table in the terminal
+function readProducts() {
+    console.log("Selecting everything from table into table within the terminal...\n");
+    connection.query("SELECT * FROM products", function(err,res) {
+        if(err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
