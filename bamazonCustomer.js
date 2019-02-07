0972a304f65360053789f64d31bb7a28d4b38f9e
losandluca <lewisalcazar88@gmail.com>
2019-02-06 22:04:17 -0700

    adding logic to display data to terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,18 @@
 //packages and database
-const inquirer = require("inquirer") ;
+const inquirer = require("inquirer");
 const mysql = require("mysql");
-const  table = require("cli-table");
+const table = require("cli-table");
 
 //connections to mysql database and
-const connection = mysql.createConnection( {
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3500,
     user: "root",
-    password:"root"
+    password: "root"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
 
     readProducts();
@@ -20,10 +20,21 @@ connection.connect(function(err) {
 
 //function to display data into table in the terminal
 function readProducts() {
-    console.log("Selecting everything from table into table within the terminal...\n");
-    connection.query("SELECT * FROM products", function(err,res) {
-        if(err) throw err;
-        console.log(res);
-        connection.end();
+    console.log("Selecting data into table within the terminal...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        //npm cli-table 
+        const dataTable = new Table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+            colWidths: [10, 25, 10, 10]
+        });
+        for(let i = 0; i <res.length; i++) {
+            dataTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quanity]
+            );
+
+            console.log(table.toString());
+        }
     });
 }
